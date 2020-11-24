4bf3e6c6cb2e98613dd7b00c81a44581ad67ab7f
mohammad0-0ahmad <kurtular.vadisi@gmail.com>
2020-11-25 00:40:36 +0100

    Change a color

--- client/constants/colors.js
+++ client/constants/colors.js
@@ -1,7 +1,7 @@
 export default {
-  color1: { light: "#202040", dark: "#f0f2f5" },
-  color2: { light: "#f0f2f5", dark: "#171728" },
-  color3: { light: "#f0f2f5", dark: "#202040" },
+  color1: { light: "#202040", dark: "#f6f6f6" },
+  color2: { light: "#f6f6f6", dark: "#171728" },
+  color3: { light: "#f6f6f6", dark: "#202040" },
   color4: { light: "#559cad", dark: "#4088f4" },
   color5: { light: "#ffffff", dark: "#202040" },
   red: { light: "#fc9d9d", dark: "#fc9d9d" },

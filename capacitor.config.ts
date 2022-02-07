e04d1c8f6f5242cb8a03743dbc958f34070c3465
mohammad0-0ahmad <kurtular.vadisi@gmail.com>
2022-02-07 03:44:03 +0100

    enable live reloading for native app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'se.todo.app',
-  appName: 'To-do',
-  webDir: 'out',
-  bundledWebRuntime: false
+    appId: 'se.todo.app',
+    appName: 'To-do',
+    webDir: 'out',
+    loggingBehavior: 'debug',
+    bundledWebRuntime: false,
+    ...(process.env?.NODE_ENV_NATIVE === 'enabled'
+        ? {}
+        : {
+              server: {
+                  url: `http://${require('ip').address()}:3000}`,
+                  cleartext: true,
+              },
+          }),
 };
 
 export default config;
